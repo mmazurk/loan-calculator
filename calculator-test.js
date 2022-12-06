2aca29c9527c47537ff070962909758510bc52cd
Mark Mazurkiewicz <markmazurk@gmail.com>
2022-12-06 09:34:38 -0800

    Zak helped me fix

--- calculator-test.js
+++ calculator-test.js
@@ -1,4 +1,8 @@
 
+// window.onload = (event) => {
+//   //code here
+// console.log("Hello")
+
 it('should calculate the monthly rate correctly', function () {
   const testvalues = {
     amount: 1600000,
@@ -9,7 +13,6 @@ it('should calculate the monthly rate correctly', function () {
   
 });
 
-
 it("should return a result with 2 decimal places", function() {
   const testvalues = {
     amount: 10043,
@@ -19,4 +22,12 @@ it("should return a result with 2 decimal places", function() {
   expect(calculateMonthlyPayment(testvalues)).toEqual('131.00')
 });
 
+// I need to check and see if it inserted the correct value into the page
+
+it('should insert the correct value into the page', function() {
+  var foo = document.getElementById('monthly-payment')
+  expect(foo.innerText).toEqual('$7301.32');
+})
+
+// };
 
